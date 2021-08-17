97a843759628577364bbf05d8d67ce405a0e5dcc
sana-hm <sana.sh19@yahoo.com>
2021-08-17 11:21:22 +0200

    add slideBar Left

---
+++ Wordpress/jquery/box-img.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    $("#metro-box").mCustomScrollbar({
+        horizontalScroll:true,
+    });
+});
+
+
+
+$(function(){
+    $('.demo').easyTicker({
+            direction: 'up'
+    });
+});
